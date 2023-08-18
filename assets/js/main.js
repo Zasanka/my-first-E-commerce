734bc3ec47684b02f0eed505f6cc750d02a45b29
Mariano Sandoval <129957094+Zasanka@users.noreply.github.com>
2023-08-18 18:41:08 -0300

    complete project

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+import cart from "./components/cart.js";
+import loader from "./components/loader.js";
+import products from "./components/products.js";
+import showCart from "./components/showCart.js";
+import showMenu from "./components/showMenu.js";
+import getProducts from "./helpers/getProducts.js";
+
+loader()
+
+showMenu()
+
+showCart()
+
+const { db, printProducts } = products(await getProducts())
+
+cart(db, printProducts)
